acf6a3b8ff7e794dcb354d942585f690c2955351
Administrator <Administrator@MURU>
2017-01-31 22:07:46 +0000

    tests tests

---
+++ WebApiTestToDoTests/UnitTest2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WebApiTestToDoTests
+{
+    /// <summary>
+    /// Summary description for UnitTest2
+    /// </summary>
+    [TestClass]
+    public class UnitTest2
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int count =10;
+            count = count * 12;
+            Assert.AreEqual(count, 120);
+
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int count = 10;
+            count = count * 12;
+            Assert.AreEqual(count, 120);
+
+        }
+
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            int count = 10;
+            count = count * 12;
+            Assert.AreEqual(count, 120);
+
+        }
+    }
+}
---
+++ WebApiTestToDoTests/UnitTest3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WebApiTestToDoTests
+{
+    /// <summary>
+    /// Summary description for UnitTest3
+    /// </summary>
+    [TestClass]
+    public class UnitTest3
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int c = 200;
+            c = c * 10;
+            Assert.AreEqual(c, 2000);
+        }
+    }
+}
